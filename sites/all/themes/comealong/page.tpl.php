e487d593e9add41320d468eedcfb64fe640ba476
Phillip Shannon <student@epicodus.com>
2015-11-11 13:25:11 -0800

    Merge with Chris, add styling for footer, add search bar as feature

--- sites/all/themes/comealong/page.tpl.php
+++ sites/all/themes/comealong/page.tpl.php
@@ -60,26 +60,28 @@
     <div id="content">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
+      <?php print render($title_suffix); ?>
 
-        <?php print render($messages); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <?php print render($messages); ?>
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
-            <?php print render($page['content']); ?>
-          </div>
+      <?php print render($page['content']); ?>
+    </div>
 
-          <?php if ($page['sidebar_first']): ?>
-            <div id="sidebar">
-              <?php print render($page['sidebar_first']); ?>
-            </div>
+      <?php if ($page['sidebar_first']): ?>
+        <div id="sidebar">
+          <?php print render($page['sidebar_first']); ?>
+        </div>
+      <?php endif; ?>
+
+
+  </div>
+      <div id="footer" class="navbar navbar-fixed-bottom">
+        <div class="container ">
+          <?php if ($page['footer']): ?>
+            <?php print render($page['footer']); ?>
           <?php endif; ?>
-
-          <div id="footer">
-            <?php if ($page['footer']): ?>
-              <?php print render($page['footer']); ?>
-            <?php endif; ?>
-          </div>
-
         </div>
       </div>
+</div>
